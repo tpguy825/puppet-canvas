f73db422f3d36a2055135e4f880732148b6823cd
Toby Payne <75158953+tpguy825@users.noreply.github.com>
2023-07-20 21:07:32 +0100

    Update puppet-canvas.d.ts

--- bin/puppet-canvas.d.ts
+++ bin/puppet-canvas.d.ts
@@ -2,11 +2,11 @@
 import { ElementHandle, Page, ScreenshotOptions, Browser } from 'puppeteer';
 export declare function linkCanvas(canvas: ElementHandle<HTMLCanvasElement>): Promise<EverythingNeedsAwaiting<HTMLCanvasElement>>;
 export declare function createCanvas(width: number, height: number, browser?: Browser): Promise<EverythingNeedsAwaiting<HTMLCanvasElement>>;
-export declare function screenshotCanvas(canvas: HTMLCanvasElement, options?: ScreenshotOptions): Promise<string | Buffer>;
-export declare function releaseCanvas(canvas: HTMLCanvasElement): Promise<void>;
+export declare function screenshotCanvas(canvas: EverythingNeedsAwaiting<HTMLCanvasElement>, options?: ScreenshotOptions): Promise<string | Buffer>;
+export declare function releaseCanvas(canvas: EverythingNeedsAwaiting<HTMLCanvasElement>): Promise<void>;
 export declare function close(): Promise<void>;
-export declare function loadFont(name: string, url: string, canvas: HTMLCanvasElement): Promise<void>;
-export declare function loadImage(url: string, canvas: HTMLCanvasElement, page?: Page): Promise<HTMLImageElement>;
+export declare function loadFont(name: string, url: string, canvas: EverythingNeedsAwaiting<HTMLCanvasElement>): Promise<void>;
+export declare function loadImage(url: string, canvas: EverythingNeedsAwaiting<HTMLCanvasElement>, page?: Page): Promise<HTMLImageElement>;
 type EverythingNeedsAwaiting<Object> = {
 	[Key in keyof Object]: Object[Key] extends Record<string | number | symbol, any> ? EverythingNeedsAwaiting<Object[Key]> : Promise<Object[Key]>;
 }
