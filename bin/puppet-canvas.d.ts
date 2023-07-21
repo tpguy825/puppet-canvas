03cf954c1fd7cccfbb134d40872ac6caaedec945
Toby Payne <75158953+tpguy825@users.noreply.github.com>
2023-07-21 07:39:29 +0100

    fixed types

--- bin/puppet-canvas.d.ts
+++ bin/puppet-canvas.d.ts
@@ -1,12 +1,42 @@
 /// <reference types="node" />
-import { ElementHandle, Page, ScreenshotOptions, Browser } from 'puppeteer';
-export declare function linkCanvas(canvas: ElementHandle<HTMLCanvasElement>): Promise<EverythingNeedsAwaiting<HTMLCanvasElement>>;
-export declare function createCanvas(width: number, height: number, browser?: Browser): Promise<EverythingNeedsAwaiting<HTMLCanvasElement>>;
-export declare function screenshotCanvas(canvas: EverythingNeedsAwaiting<HTMLCanvasElement>, options?: ScreenshotOptions): Promise<string | Buffer>;
+import { ElementHandle, Page, ScreenshotOptions, Browser } from "puppeteer";
+export declare function linkCanvas(
+	canvas: ElementHandle<HTMLCanvasElement>,
+): Promise<EverythingNeedsAwaiting<HTMLCanvasElement>>;
+export declare function createCanvas(
+	width: number,
+	height: number,
+	browser?: Browser,
+): Promise<EverythingNeedsAwaiting<HTMLCanvasElement>>;
+export declare function screenshotCanvas(
+	canvas: EverythingNeedsAwaiting<HTMLCanvasElement>,
+	options?: ScreenshotOptions,
+): Promise<string | Buffer>;
 export declare function releaseCanvas(canvas: EverythingNeedsAwaiting<HTMLCanvasElement>): Promise<void>;
 export declare function close(): Promise<void>;
-export declare function loadFont(name: string, url: string, canvas: EverythingNeedsAwaiting<HTMLCanvasElement>): Promise<void>;
-export declare function loadImage(url: string, canvas: EverythingNeedsAwaiting<HTMLCanvasElement>, page?: Page): Promise<HTMLImageElement>;
-type EverythingNeedsAwaiting<Object> = Object extends string | number | symbol | bigint | boolean | null | undefined ? Promise<Object> : Object extends (...args: any) => any ? (...args: Parameters<Object>) => Promise<ReturnType<Object>> : {
-	[Key in keyof Object]: Object[Key] extends Record<string | number | symbol, any> ? EverythingNeedsAwaiting<Object[Key]> : Promise<Object[Key]>;
-}
+export declare function loadFont(
+	name: string,
+	url: string,
+	canvas: EverythingNeedsAwaiting<HTMLCanvasElement>,
+): Promise<void>;
+export declare function loadImage(
+	url: string,
+	canvas: EverythingNeedsAwaiting<HTMLCanvasElement>,
+	page?: Page,
+): Promise<HTMLImageElement>;
+type EverythingNeedsAwaiting<Object> = Object extends
+	| string
+	| number
+	| symbol
+	| bigint
+	| boolean
+	| null
+	| undefined
+	? Promise<Object>
+	: Object extends (...args: any) => any
+	? (...args: Parameters<Object>) => Promise<ReturnType<Object>>
+	: {
+			[Key in keyof Object]: Object[Key] extends Record<string | number | symbol, any>
+				? EverythingNeedsAwaiting<Object[Key]>
+				: Promise<Object[Key]>;
+	  };
